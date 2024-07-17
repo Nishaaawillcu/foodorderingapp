67f70c9cacd6618e80281dc24c4700971abf54c9
sugamxz <sugamkhanal38@gmail.com>
2024-07-17 21:05:06 +0545

    okk

--- FoodOrderingSystem.java
+++ FoodOrderingSystem.java
@@ -7,7 +7,7 @@ public class FoodOrderingSystem {
             // Initialize with dummy accounts
             AdminFrame adminFrame = new AdminFrame("admin");
             adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            adminFrame.setVisible(false); // Temporarily set invisible to initialize accounts
+            adminFrame.setVisible(false); // Temporarily set invisible to initialize accounts okk
 
             // Open login frame
             LoginFrame loginFrame = new LoginFrame();
